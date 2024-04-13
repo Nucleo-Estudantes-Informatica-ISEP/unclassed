4b5c5bbdad7410d185e4f74e18f12c87eab83170
André L Reis <gato5andre26@gmail.com>
2024-04-13 15:05:00 +0100

    feat: socket impl

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,7 @@
 import { cookies } from "./cookies";
+import { sockets } from "./socket"
 
 export const config = {
   cookies,
+  sockets
 };
---
+++ src/config/socket.ts
@@ -0,0 +1,3 @@
+export const sockets = {
+    url: process.env.NEXT_PUBLIC_SOCKET_SERVER_URL ?? "http://localhost:7000",
+};
